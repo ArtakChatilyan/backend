64a937e348a8b5c2dc5b30140bf9ea12eadf5f35
Artak <artak.chatilyan@gmail.com>
2025-06-05 13:07:55 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+const errorMiddleware=require('./middlewares/error-middleware');
+
+// const dataAccess = require("./database");
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(errorMiddleware);
+
+const start = () => {
+  try {
+    app.listen(8080, () => {
+      console.log("Server is running on port 8080");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
